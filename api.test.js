a6b09cdbbd7cdc82bd159db9597bfcb0724fdfbc
Mohanish <manushevde95@gmail.com>
2023-07-16 16:04:11 +0530

    created separate test file all test working

--- api.test.js
+++ api.test.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const app = require('./main'); 
 
 const dataFilePath = './tasks.json';
+const testFilePath = './tasks-test.json'
 
 beforeEach(() => {
   if (fs.existsSync(dataFilePath)) {
@@ -11,52 +12,60 @@ beforeEach(() => {
   fs.writeFileSync(dataFilePath, '[]');
 });
 
+// Working 
 test('GET', async () => {
   const response = await request(app).get('/tasks');
   expect(response.status).toBe(200);
   expect(response.body).toEqual([]);
 });
 
-test('GET with the given ID', async () => {
-  const newItem = { id: 1, name: 'Item 1' };
-  fs.writeFileSync(dataFilePath, JSON.stringify([newItem]));
+test('GET', async () => {
+  const newItem = { id: 1, name: 'Item 1', description : 'Item 1' };
+  fs.writeFileSync(testFilePath, JSON.stringify([newItem]));
 
   const response = await request(app).get('/tasks/1');
   expect(response.status).toBe(200);
   expect(response.body).toEqual(newItem);
 });
 
-test('PUT', async () => {
-  const newTasks = { id: 1, name: 'Item 1' };
-  fs.writeFileSync(dataFilePath, JSON.stringify([newTasks]));
-
-  const updatedTasks = { name: 'Updated Tasks' };
-
-  const response = await request(app)
-    .put('/tasks/id')
-    .send(updatedTasks);
-  expect(response.status).toBe(200);
-  const storedItems = JSON.parse(fs.readFileSync(dataFilePath));
-  expect(storedItems).toHaveLength(1);
-  expect(storedItems[0]).toEqual(expect.objectContaining(updatedTasks));
-});
-
-
 test('POST', async () => {
   const newItem = { name: 'New Item' };
   
-  const response = await request(app).post('/api/items').send(newItem);
+  const response = await request(app).post('/tasks').send(newItem);
   expect(response.status).toBe(201);
   
-  const storedItems = JSON.parse(fs.readFileSync(dataFilePath));
+  const storedItems = JSON.parse(fs.readFileSync(testFilePath));
   expect(storedItems).toHaveLength(1);
-  expect(storedItems[0]).toEqual(expect.objectContaining(newItem));
 });
 
 
+test('PUT /tasks/:taskId should update a specific task', async () => {
+  
+  const taskId = 1;
+  const originalTask = { id: taskId, title: 'Task 1', description: 'Description 1' };
+  fs.writeFileSync(testFilePath, JSON.stringify([originalTask]), 'utf8');
+
+  const updatedTask = { title: 'Updated Task', description: 'Updated Description' };
+
+  const response = await request(app)
+    .put(`/tasks/${taskId}`)
+    .send(updatedTask);
+
+  expect(response.status).toBe(200);
+  expect(response.body.title).toBe(updatedTask.title);
+  expect(response.body.description).toBe(updatedTask.description);
+
+  
+  const tasksData = fs.readFileSync(dataFilePath, 'utf8');
+  const tasks = JSON.parse(tasksData);
+  expect(tasks.length).toBe(1);
+  expect(tasks[0].title).toBe(updatedTask.title);
+  expect(tasks[0].description).toBe(updatedTask.description);
+});
+
 test('DELETE', async () => {
-  const newItem = { id: 1, name: 'Item 1' };
-  fs.writeFileSync(dataFilePath, JSON.stringify([newItem]));
+  const newItem = { id: 1, name: 'Item 1', description: 'Item' };
+  fs.writeFileSync(testFilePath, JSON.stringify([newItem]));
   
   const response = await request(app).delete('/tasks/1');
   expect(response.status).toBe(200);
@@ -64,3 +73,5 @@ test('DELETE', async () => {
   const storedItems = JSON.parse(fs.readFileSync(dataFilePath));
   expect(storedItems).toHaveLength(0);
 });
+
+
