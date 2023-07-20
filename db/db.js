47b17fecf182af91709a926e53fedc16465d4659
Mohanish <manushevde95@gmail.com>
2023-07-20 12:05:58 +0530

    removed connection string env file

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const pgp = require('pg-promise')();
 
-const cn = "postrgresql://postgres:postgres123@localhost:5432/first_db"
+require('dotenv').config();
+const cn = process.env.DB_CONNECTION_STRING;
 
 const db  = pgp(cn);
 
