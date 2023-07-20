9eff2451b076c5ecf885467a4ad9f4c806243a7b
Mohanish <manushevde95@gmail.com>
2023-07-20 12:58:43 +0530

    Modified .gitignore to exclude .env file

--- db/db.js
+++ db/db.js
@@ -1,9 +1,7 @@
 const pgp = require('pg-promise')();
 
-// const cn = "postrgresql://postgres:postgres123@localhost:5432/first_db"
 require('dotenv').config();
 const cn = process.env.DB_CONNECTION_STRING;
-// const cn = process.env.CONNECT;
 
 const db  = pgp(cn);
 
