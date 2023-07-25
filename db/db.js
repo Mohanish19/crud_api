8d36d61542d6cf424f902f914436a0798f39b0b8
Mohanish <manushevde95@gmail.com>
2023-07-25 12:59:33 +0530

    added test database

--- db/db.js
+++ db/db.js
@@ -1,8 +1,39 @@
+// const pgp = require('pg-promise')();
+
+// require('dotenv').config();
+// const cn = process.env.DB_CONNECTION_STRING;
+
+// const db  = pgp(cn);
+
+// module.exports = db;
+
+
 const pgp = require('pg-promise')();
+const dotenv = require('dotenv')
 
-require('dotenv').config();
-const cn = process.env.DB_CONNECTION_STRING;
+dotenv.config()
 
-const db  = pgp(cn);
+const productionConnectionURI = process.env.PRODUCTION_DB_URI;
+const testingConnectionURI = process.env.TESTING_DB_URI;
 
-module.exports = db;
+
+let db;
+
+function initDB() {
+  if (process.env.NODE_ENV === 'production') {
+    db = pgp(productionConnectionURI);
+  } else if (process.env.NODE_ENV === 'test') {
+    db = pgp(testingConnectionURI);
+  } else {
+    throw new Error('Invalid NODE_ENV. Set NODE_ENV to "production" or "test".');
+  }
+}
+
+function getDB() {
+  if (!db) {
+    initDB();
+  }
+  return db;
+}
+
+module.exports = getDB();
