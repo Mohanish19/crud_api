87536809be75d3fe44655678a04ad05881f6bb2b
Mohanish <manushevde95@gmail.com>
2023-08-07 17:26:15 +0530

    added pl/pgsql functions for http methods

--- practice.sql
+++ practice.sql
@@ -38,35 +38,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- get method
-
-CREATE OR REPLACE FUNCTION get_tasks(limit integer, skip integer, order_by text, order text, group_by text DEFAULT NULL)
-RETURNS TABLE (id integer, title text, description text, created_time timestamp, status text, task_id integer, parent_subtask_id integer, task_count integer)
-AS $$
-BEGIN
-  IF group_by = 'status' THEN
-    -- Get tasks grouped by status
-    RETURN QUERY
-    SELECT status, COUNT(*) AS task_count
-    FROM subtasks
-    GROUP BY status
-    ORDER BY CASE WHEN order_by = 'created_time' OR order_by = 'updated_time' THEN order_by END || ' ' || order
-    LIMIT limit
-    OFFSET skip;
-  ELSE
-    -- Get tasks without grouping
-    RETURN QUERY
-    SELECT *
-    FROM subtasks
-    ORDER BY CASE WHEN order_by = 'created_time' OR order_by = 'updated_time' THEN order_by END || ' ' || order
-    LIMIT limit
-    OFFSET skip;
-  END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
---  get/id methofd
+--  get/id method
 
 CREATE OR REPLACE FUNCTION get_task_with_subtasks(task_id integer)
 RETURNS TABLE (id integer, title text, description text, created_time timestamp, status text, task_id integer, parent_subtask_id integer)
@@ -84,31 +56,6 @@ BEGIN
     FROM subtasks_recursive sr JOIN subtasks s ON sr.id = s.parent_subtask_id
   )
   SELECT * FROM subtasks_recursive;
-END;
-$$ LANGUAGE plpgsql;
-
-
--- put method
-
-CREATE OR REPLACE FUNCTION update_task(id integer, title text, description text, status text, parent_subtask_id integer DEFAULT NULL)
-RETURNS TABLE (id integer, title text, description text, created_time timestamp, updated_time timestamp, status text, task_id integer, parent_subtask_id integer)
-AS $$
-BEGIN
-  IF parent_subtask_id = 'PROMOTE' THEN
-    -- Promote the subtask to an independent task (remove parent_subtask_id)
-    RETURN QUERY
-    UPDATE subtasks
-    SET title = title, description = description, updated_time = NOW(), status = status, parent_subtask_id = NULL
-    WHERE id = id
-    RETURNING *;
-  ELSE
-    -- Check if the task or subtask exists in the subtasks table
-    RETURN QUERY
-    UPDATE subtasks
-    SET title = title, description = description, updated_time = NOW(), status = status, parent_subtask_id = parent_subtask_id
-    WHERE id = id AND (parent_subtask_id IS NULL OR parent_subtask_id = parent_subtask_id)
-    RETURNING *;
-  END IF;
 END;
 $$ LANGUAGE plpgsql;
 
